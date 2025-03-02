315a5330fc480573e4ae7da5ff36493c4e5e7e61
MaxMishaev7 <100563472+MaxMishaev7@users.noreply.github.com>
2025-03-02 10:57:22 +0300

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,7 @@
+public class Main1 {
+    public static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+        System.out.println("Итого (руб.):\n" + (income - spending));
+    }
+}
